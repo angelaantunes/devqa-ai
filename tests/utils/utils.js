f0d99566612db5b4230699ac0129a130634c40c1
angelaantunes <66823466+angelaantunes@users.noreply.github.com>
2025-10-23 10:52:34 +0100

    Update utils.js for test #6

--- tests/utils/utils.js
+++ tests/utils/utils.js
@@ -3,3 +3,21 @@ export async function login(page, username, password) {
   await page.getByPlaceholder('Password').fill(password);
   await page.getByRole('button', { name: 'Login' }).click();
 }
+
+export async function addFirstItemToCart(page) {
+  const addButton = page.locator('[data-test="inventory-item"] >> [data-test^="add-to-cart"]').first();
+  await addButton.click();
+  await expect(addButton).toHaveText('Remove');
+}
+
+export async function goToCart(page) {
+  await page.click('[data-test="shopping-cart-container"]');
+  await expect(page).toHaveURL(/.*cart.html/);
+}
+
+export async function getCartItemPrice(page, index = 0) {
+  return page
+    .locator('[data-test="cart-item"] >> [data-test="inventory-item-price"]')
+    .nth(index)
+    .textContent();
+}
