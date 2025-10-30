dfa96efad1547eec5ea4a6cfdcb828638fb5a0f7
angelaantunes <66823466+angelaantunes@users.noreply.github.com>
2025-10-30 12:15:55 +0000

    Update utils.js for test #1

---
+++ backend/tests/utils/utils.js
@@ -0,0 +1,7 @@
+// Exported helpers: login
+
+export async function login(page, username, password) {
+  await page.getByPlaceholder('Username').fill(username);
+  await page.getByPlaceholder('Password').fill(password);
+  await page.getByRole('button', { name: 'Login' }).click();
+}
