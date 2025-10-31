52b3485f6ebe9da9d1d959aef834d875372b65a9
alegna83 <angelalopesantunes@gmail.com>
2025-10-31 12:00:40 +0000

    clean tests

--- backend/tests/generated/test_login_successful.spec.js
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-import { login } from '../utils/utils.js';
-
-test('Test Login Successful', async ({ page }) => {
-  await page.goto('https://www.saucedemo.com/');
-  await login(page, 'standard_user', 'secret_sauce');
-  await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
-});
