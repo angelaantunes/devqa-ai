f505ab36263cc576aae25bdd7d035576878cfb70
angelaantunes <66823466+angelaantunes@users.noreply.github.com>
2025-11-12 10:51:56 +0000

    Add/Update test for issue #1: Test Login Successful

--- backend/tests/generated/test_login_successful.spec.js
+++ backend/tests/generated/test_login_successful.spec.js
@@ -1,8 +1,8 @@
 import { test, expect } from '@playwright/test';
 import { login } from '../utils/utils.js';
 
-test('successful login with test_user', async ({ page }) => {
+test('Test Login Successful', async ({ page }) => {
   await page.goto('https://www.saucedemo.com/');
-  await login(page, 'test_user', 'secret_sauce');
+  await login(page, 'standard_user', 'secret_sauce');
   await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
 });
