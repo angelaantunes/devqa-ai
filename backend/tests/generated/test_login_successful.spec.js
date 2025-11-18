d40b54cc82e3f0942f445a4549afe14d9fdd00aa
angelaantunes <66823466+angelaantunes@users.noreply.github.com>
2025-11-18 11:50:44 +0000

    Add/Update test for issue #1: Test Login Successful

--- backend/tests/generated/test_login_successful.spec.js
+++ backend/tests/generated/test_login_successful.spec.js
@@ -1,9 +1,11 @@
 import { test, expect } from '@playwright/test';
 import { login } from '../utils/utils.js';
 
-test('Login with standard_user should redirect to inventory page', async ({ page }) => {
-  await page.goto('https://www.saucedemo.com/');
-  await login(page, 'standard_user', 'secret_sauce');
-  await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
-  await expect(page.locator('[data-test="title"]')).toHaveText('Products');
+
+test.describe("successful login with standard_user", () => {
+  test('successful login with standard_user', async ({ page }) => {
+    await page.goto('https://www.saucedemo.com/');
+    await login(page, 'standard_user', 'secret_sauce');
+    await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
+  });
 });
